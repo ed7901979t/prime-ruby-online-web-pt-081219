6143bce264103c7b95409a8424d0eb64498a16b4
clean-command-1851 <ed7901979t@gmail.com>
2019-08-20 11:13:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,13 @@ num = 9
 def prime?(num)
   if (num==2) 
     return true 
-      elsif ((num ** 1)%2!=0)
+      elsif ((num ** 1)%2!=0) 
         return true
       elsif
         n <= 1
         n * -1
           return "Not a prime number"
-      elsif ((num ** 1)%2==0)
+      elsif ((num ** 1)%2==0) || ((num(-1)**1)%2==0))
         return "Not a primary number"
       else
         return false
