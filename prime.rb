11828be13b37d610a9daa80fea93669a2281ce81
clean-command-1851 <ed7901979t@gmail.com>
2019-08-20 10:43:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+num = 9
+def primemethod(num)
+  if (num==2) 
+    return true 
+    elsif ((num ** 1)%2!=0)
+    return true
+    else 
+    return false
+    end
+end
+
+primemethod(num)
+
